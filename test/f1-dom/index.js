14ccf5f7692f4ac9939e0ae1b98f865de530a191
Mikko Haapoja <me@mikkoh.com>
2016-03-31 13:39:47 -0400

    Setup running easier through npm test

--- test/f1-dom/index.js
+++ test/f1-dom/index.js
@@ -1,5 +1,5 @@
 var aeToF1 = require('../../src/');
-var f1Dom = require('../../src/converter/f1-dom');
+var f1Dom = require('../../src/statesConverter/f1-dom');
 var json = require('../ae-export');
 var toJSString = require('serialize-javascript');
 var fs = require('fs');
@@ -13,8 +13,12 @@ fs.writeFileSync(path.join(__dirname, 'statesTransitions.js'), statesTransitions
 budo(
   path.join(__dirname, 'frontend.js'),
   {
-    live: true,
     dir: path.join(__dirname, '..', 'ae', '(Footage)', 'Images'),
-    open: true
+    open: true,
+    port: 8000
   }
-);
+)
+.on('connect', function() {
+  console.log('up and runnning on :8000');
+});
+
