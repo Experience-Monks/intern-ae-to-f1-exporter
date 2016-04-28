14bda334bc07566793ce3b2d5bfc533e303a380d
Mikko Haapoja <me@mikkoh.com>
2016-04-28 11:16:02 -0400

    Fixed issue where transitions in reverse would not run

--- test/f1-dom/frontend.js
+++ test/f1-dom/frontend.js
@@ -1,6 +1,10 @@
 var getUI = require('./test-f1-dom');
 
 var elUI = document.body.appendChild(document.createElement('div'));
+elUI.style.width = '400px';
+elUI.style.height = '640px';
+elUI.style.overflow = 'hidden';
+
 var ui = getUI({
   el: elUI
 });
@@ -9,11 +13,11 @@ ui.init('out');
 ui.go('idle');
 
 window.addEventListener('mousedown', function() {
-  console.log('over');
+  console.log('----- over');
   ui.go('over');
 });
 
 window.addEventListener('mouseup', function() {
-  console.log('idle');
+  console.log('---- idle');
   ui.go('idle');
 });
