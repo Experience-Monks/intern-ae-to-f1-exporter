84e16780cf9e070d9ee31ce4e6893b95d659704d
nascherman <n_scherman@hotmail.com>
2016-06-22 13:13:27 -0400

    support for multiple components in a project

--- exporters/react-f1/index.js
+++ exporters/react-f1/index.js
@@ -8,6 +8,7 @@ var getTargets = require('../../src/getTargets');
 var getHTML = require('./getHTML');
 var copyAssetsFromTargets = require('../../src/copyAssetsFromTargets');
 var outputAnimationJSON = require('../../src/exporter/outputAnimationJSON');
+var getMultiComp = require('../../src/util/getMultiComp');
 
 module.exports = function(opts) {
   if(opts.pathJSON === undefined) {
@@ -21,13 +22,36 @@ module.exports = function(opts) {
   // read in data needed for export
   var json = JSON.parse(fs.readFileSync(opts.pathJSON));
 
-  // convert after effects json data to something useable
+  var components = getMultiComp(json);
+  // multiple components
+  if(components.length > 1 ) {
+    components.forEach(function(comp) {
+      var outPath = path.join(opts.pathOut, comp.name);
+      fs.mkdirSync(outPath);
+      generateIndividualComp(outPath, comp, json);
+    });
+  }
+  else {
+    // convert after effects json data to something useable
+    var animationData = aeToF1(json);
+  
+      outputAnimationJSON(opts, json, animationData);
+      outputTargets(opts, json, animationData);
+      outputIndexJS(opts, json, animationData);    
+  }
+
+};
+
+function generateIndividualComp(path, comp, json) {
+  json.project.items = comp.items;
   var animationData = aeToF1(json);
+  var opts = {};
+  opts.pathOut = path + '/';
 
   outputAnimationJSON(opts, json, animationData);
   outputTargets(opts, json, animationData);
-  outputIndexJS(opts, json, animationData);
-};
+  outputIndexJS(opts, json, animationData);    
+}
 
 function outputTargets(opts, json, animationData) {
   var targets = getTargets(json);
---
+++ src/util/getMultiComp.js
@@ -0,0 +1,25 @@
+module.exports = function(json) {
+	if(!json) throw new Error('A json file is required');
+	if(!json.project) thrown new Error('Json input is not in the correct format');
+
+	var project = json.project;
+	var comps = [];
+
+	//find top level folder structure
+	project.items.forEach(function(item) {
+		
+		if(item.typeName === 'Folder') {
+			var foundComponent = true;
+			if(item.items) {
+				item.items.forEach(function(item) {
+					if(item.typeName !== 'Folder') {
+						foundComponent = false;
+					}
+				});
+				comps.push(item);
+			}
+		}
+	});
+
+	return comps;
+};
