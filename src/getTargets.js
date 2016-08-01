8fbaa83447d633d717ecb049e63a7f8408013504
Nick Scherman <n_scherman@hotmail.com>
2016-08-01 17:25:34 -0400

    windows font prefixes

--- src/getTargets.js
+++ src/getTargets.js
@@ -2,11 +2,11 @@ var getCompositions = require('./util/getCompositions');
 var getFirstTransitionComposition = require('./util/getFirstTransitionComposition');
 var getTargetName = require('./util/getTargetName');
 var fs = require('fs');
+var os = require('os');
 
 var fontPrefixes = ['.otf', '.ttf', '.ttc', '.dfont'];
 var osxPrefixes = ['/Library/Fonts/', '/System/Library/Fonts/'];
-// TODO windows system font prefixes
-// var windowsPrefixes...
+var windowsPrefixes = [process.env.windir + '\\fonts']
 
 module.exports = function(json) {
   var rVal = null;
@@ -30,12 +30,22 @@ module.exports = function(json) {
       else if(Object.keys(layer.font).length > 0) {
         var fontPath;
         //get font from osx library location - TODO windows font copy
-        osxPrefixes.forEach(function(pre) {
-          fontPrefixes.forEach(function(f) {
-            var found = fs.existsSync(pre + layer.font.font + f)
-            if(found) fontPath = pre + layer.font.font + f;  
-          })
-        });
+        if(os.platform() === 'darwin') {
+          osxPrefixes.forEach(function(pre) {
+            fontPrefixes.forEach(function(f) {
+              var found = fs.existsSync(pre + layer.font.font + f)
+              if(found) fontPath = pre + layer.font.font + f;  
+            })
+          });  
+        }
+        if(os.platform() === 'win32') {
+          windowsPrefixes.forEach(function(pre) {
+            fontPrefixes.forEach(function(f) {
+              var found = fs.existsSync(pre + '\\' + layer.font.font + f)
+              if(found) fontPath = pre + '\\' + layer.font.font + f;  
+            })
+          });
+        }
         if(!fontPath) throw new Error('The text layer font face could not be found on your system');
         
         layer.source = fontPath;
